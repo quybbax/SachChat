c7c8abab56a44b8a1eee1ba6866b4f7b4ebc2d92
ngogiaquy <ngogiaquy3@gmail.com>
2025-04-27 17:50:18 +0700

    đưa giao diện ra thư viện

--- lichsu.py
+++ lichsu.py
@@ -7,28 +7,11 @@
 import streamlit as st
 from thuvien import sach
 from thuvien import chat
-from thuvien.sach import nap_csdl
+from thuvien import giaodien
 
 cacbaihoc = sach.lay_sach("thuvien/dulieu/lop8/lichsu.txt")
 csdl = sach.nap_csdl(cacbaihoc,"thuvien/dulieu/lop8/lichsu.data")
 
 
 def run():
-    st.title("Lịch sử")
-    # đoạn lệnh này sẽ hiển thị những thông tin trong phần lịch sử chat
-    if "danhsachtinnhan" not in st.session_state:
-        st.session_state.danhsachtinnhan = []
-    for tin_nhan in st.session_state.danhsachtinnhan:
-        st.chat_message(tin_nhan["vaitro"]).markdown(tin_nhan["noidung"])
-
-    cau_hoi = st.chat_input("Hãy nói điều gì đó")
-
-    if cau_hoi:
-        st.chat_message("user").markdown(cau_hoi)
-        baihoc = sach.lay_bai_hoc(cacbaihoc, csdl, cau_hoi)
-        tra_loi = chat.hoi_deepseek(cau_hoi, baihoc)
-        st.chat_message("ai").markdown(tra_loi)
-        lichsuchat1 = {"vaitro": "user", "noidung": cau_hoi}
-        st.session_state.danhsachtinnhan.append(lichsuchat1)
-        lichsuchat2 = {"vaitro": "ai", "noidung": tra_loi}
-        st.session_state.danhsachtinnhan.append(lichsuchat2)
+    giaodien.giao_dien_chat("Lịch sử", cacbaihoc, csdl)
---
+++ thuvien/giaodien.py
@@ -0,0 +1,28 @@
+# giaodien.py
+#
+# Copyright (C) 2025 Ngô Gia Quý
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License...
+
+import streamlit as st
+from . import sach
+from . import chat
+
+def giao_dien_chat(title, cacbaihoc, csdl):
+    st.title(title)
+    if "danhsachtinnhan" not in st.session_state:
+        st.session_state.danhsachtinnhan = []
+    for tin_nhan in st.session_state.danhsachtinnhan:
+        st.chat_message(tin_nhan["vaitro"]).markdown(tin_nhan["noidung"])
+
+    cau_hoi = st.chat_input("Hãy nhập câu hỏi của bạn")
+
+    if cau_hoi:
+        st.chat_message("user").markdown(cau_hoi)
+        baihoc = sach.lay_bai_hoc(cacbaihoc, csdl, cau_hoi)
+        tra_loi = chat.hoi_deepseek(cau_hoi, baihoc)
+        st.chat_message("ai").markdown(tra_loi)
+        lichsuchat1 = {"vaitro": "user", "noidung": cau_hoi}
+        st.session_state.danhsachtinnhan.append(lichsuchat1)
+        lichsuchat2 = {"vaitro": "ai", "noidung": tra_loi}
+        st.session_state.danhsachtinnhan.append(lichsuchat2)
